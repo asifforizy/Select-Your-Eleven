46efdbcf75449d31eafc381af60fff30c8fe370e
MD Saifur Rahaman <asifforizy@gmail.com>
2026-01-28 20:05:06 +0600

    navbar completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+
+const App = () => {
+  return (
+    <div className='lg:w-10/12 mx-auto' >
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default App;
